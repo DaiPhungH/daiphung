07bd795f3748710be9c9533acec24bf8b6209a2b
Admin <daiphung79@gmail.com>
2024-10-08 20:53:54 +0700

    first commit1

---
+++ src/test2/bphuong.java
@@ -0,0 +1,13 @@
+package test2;
+
+import java.util.Scanner;
+
+public class bphuong {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Nhap vao so can tinh: ");
+        int so = nhap.nextInt();
+        System.out.printf("%d", so*so);
+    }
+
+}
