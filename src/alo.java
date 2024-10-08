fc68870dadac147b49e06283e3efbaaccdf10e8c
unknown <daiphung79@gmail.com>
2024-10-08 19:49:57 +0700

    first commit1

--- src/alo.java
+++ src/alo.java
@@ -4,5 +4,11 @@ public class alo {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // Call the second method
+        helloWorld2();
+    }
+
+    public static void helloWorld2() {
+        System.out.println("Hello World 2");
     }
 }
